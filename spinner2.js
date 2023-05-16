7cae55f1edd507bfcc423608e350fee8e7eea40a
workuseifu1 <workuseifu1@gmail.com>
2023-05-16 14:20:43 -0500

    refactor spinner2.js

--- spinner2.js
+++ spinner2.js
@@ -1,13 +1,9 @@
 const char = ['\r|  ', '\r/  ', '\r-  ', '\r\\  ', '\r|  ', '\r/  ', '\r-  ', '\r\\  ', '\r|  '];
 for (let i = 0; i < char.length; i++) {
-  let speed = 100 + 200 * i;
-  if (i !== char.length - 1) {
-    setTimeout(() => {
-      process.stdout.write(char[i]);
-    }, speed);
-  } else {
-    setTimeout(() => {
-      process.stdout.write(char[i] + "\n");
-    }, speed);
-  }
-}
+  let speed = 100 + 200 * i;  
+  (i !== char.length -1) ? setTimeout(() => {
+    process.stdout.write(char[i]);
+  }, speed) : setTimeout(() => {
+    process.stdout.write(char[i] + "\n");
+  }, speed);  
+};
